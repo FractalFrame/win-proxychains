045648799249408e3c5035b38a6032686f4b046f
contact@fractalframe.eu <fake@fake>
2026-09-15 23:25:48 +0200

    build fixes.

--- build.bat
+++ build.bat
@@ -1,2 +1,9 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+
 cargo build --release -p win-proxychains --features std-bin --bin win-proxychains
+if errorlevel 1 exit /b %errorlevel%
+
 cargo build --release -p win-proxychains --lib
+exit /b %errorlevel%
